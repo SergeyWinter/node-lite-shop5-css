44a89dd3819616c0b3ddb3bd341c749d1c389b91
SergeyWinter <32358568+SergeyWinter@users.noreply.github.com>
2022-07-26 11:56:39 +0500

    skidon2 на skidon
    skidon

--- app/public/js/card22.js
+++ app/public/js/card22.js
@@ -74,7 +74,7 @@ function ajaxGetGoodsInfo(){
 		// console.log(JSON.parse(body));
     // console.log(body);
     showCart(JSON.parse(body));
-    skidon2(JSON.parse(body));
+    skidon(JSON.parse(body));
 	})
 }
 // Data-то что влетело в body
@@ -139,33 +139,8 @@ function formatPrice(price) {
   return price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ');
 }
 ///-------------скидки ------------------------
+
 function skidon(data){
-  let out2 = '<table class="table table-striped table-cart"><tbody>';
-let total2 = 0;
-// cart берется из 19 строки функции addToCart
-  for (let key in cart){
-    // console.log(data[key]['cost']);
-  // out +=`<tr><td colspan="4"><a href="/goods?id=${key}">${data[key]['name']}</a></td></tr>`;
-  // out += `<tr><td><i class="far fa-minus-square cart-minus" data-goods_id="${key}">-</i></td>`;
-  // // cart[key]-количество
-  // out += `<td>${cart[key]}</td>`;
-  // out += `<td><i class="far fa-plus-square cart-plus" data-goods_id="${key}">+</i></td>`;
-  // out +=`<td>${data[key]['cost']*cart[key] } rub </td>`
-  // out +=`</tr>`
-  total2 += cart[key]*data[key]['cost'];
-  if (total2>3000&&total2<10000) {
-   total2=(total2*0.6);}
-  if (total2>10000&&total2<18000) {
-  total2=(total2*0.2);}
-  if (total2>18000){
-    total2=(total2*0.3);}
-  total2 += total2;
-}
-  out2 += `<tr><td colspan="3">Total2: </td><td>${formatPrice(total2)} rub</td></tr>`;
-  out2 += '</tbody></table>';
-  document.querySelector('#cart-nav2').innerHTML = out2;
-}
-function skidon2(data){
   let out22 = '<table class="table table-striped table-cart"><tbody>';let total22 = 0;
   for (let key in cart){
   total22 += cart[key]*data[key]['cost'];
